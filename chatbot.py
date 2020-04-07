d8453078112df006a5720b5251f91a0bd893ee3e
VyDee <38266371+VyDee@users.noreply.github.com>
2020-04-07 13:50:59 -0500

    first commit

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+# advice
+# Create a new chat bot named Charlie
+bot = ChatBot('Fish')
+bot.storage.drop()
+trainer = ListTrainer(bot)
+for _file in os.listdir('chat'):
+    chats = open('chat/'+ _file, encoding="utf8").readlines()
+    trainer.train(chats)
+
+#clear storage
+
+
+while True:
+    request = input('You: ')
+    response = bot.get_response(request)
+    print('Bot: ', response)
